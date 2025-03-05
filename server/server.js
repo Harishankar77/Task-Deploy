84cc1f3e052016afe0752a05bf6a329175f5b29d
Harishankar Banjare <harishankarlikeyou@gmail.com>
2025-03-05 16:12:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5175",
+  origin: "https://inext-frontend.onrender.com",
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
