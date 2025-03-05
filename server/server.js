e3f4b699160dcace5ab3f5735bce2a9a3d976b92
Harishankar77 <harishankarlikeyou@gmail.com>
2025-03-05 15:53:11 +0530

    iNEXT-Deploy

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5175",
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
